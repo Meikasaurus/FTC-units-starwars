494f91c0295f6c127c622976e607dcbb75a6e420
Guus Dohmen <guusdohmen@hotmail.nl>
2019-11-01 22:04:17 +0100

    fixed json write

--- app/src/main/java/meike/com/ftc_unitsapp/MainActivity.java
+++ app/src/main/java/meike/com/ftc_unitsapp/MainActivity.java
@@ -1,10 +1,16 @@
 package meike.com.ftc_unitsapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Debug;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,17 +18,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     private View view;
-
+    boolean isRed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
     }
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
@@ -31,7 +40,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        Vector2 a = InputCheck(message);
+        try {
+            WriteToFile(a, !isRed);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        intent.putExtra(EXTRA_MESSAGE, message + " saved to the filesystem");
         startActivity(intent);
     }
 
@@ -41,78 +56,98 @@ public class MainActivity extends AppCompatActivity {
         switch (Input) {
             case "bt":
                 pos = new Vector2(590,0);
+                isRed = false;
                 break;
             case "ba":
                 pos = new Vector2(1180,0);
+                isRed = false;
                 break;
             case "bc":
                 pos = new Vector2(2360,0);
+                isRed = false;
                 break;
             case "rt":
                 pos = new Vector2(590,0);
+                isRed = true;
                 break;
             case "ra":
                 pos =  new Vector2(1180,0);
+                isRed = true;
                 break;
             case "rc":
                 pos = new Vector2(2360,0);
+                isRed = true;
                 break;
                 default:
+
                     pos = new Vector2(0,0);
+                    isRed = false;
                     break;
         }
         return pos;
     }
-    public void WriteToFile (Vector2 Input) throws JSONException {
+    public void WriteToFile (Vector2 Input, boolean isBlue) throws JSONException {
         JSONObject JSONinput;
         JSONinput = new JSONObject();
+        JSONinput.put("blueAlliance", isBlue);
         JSONinput.put("x", Input.X);
         JSONinput.put("y", Input.Y);
-        Files.write(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOCUMENTS), JSONinput.toString().getBytes());
+
+        File file = new File(getPublicAlbumStorageDir("FTCunits"), "startpos.json" );
+        if (!file.canWrite()){
+            Log.println(1,"Error","File not created properly");
+        }
+        try {
+            FileWriter a = new FileWriter(file);
+            a.write(JSONinput.toString());
+            a.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
-
     /**gets the public image folder */
     public  static File getPublicAlbumStorageDir(String albumName) {
         // Get the directory for the user's public pictures directory.
-        File file = new File(, albumName);
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), albumName);
         if (!file.mkdirs()) {
-
+            Log.println(1, "Status","Failed to create directory");
         }
         return file;
     }
 
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////               import van stackoverflow
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
-        return false;
-    }
-
-    // Here are some examples of how you might call this method.
-// The first parameter is the MIME type, and the second parameter is the name
-// of the file you are creating:
-//
-// createFile("text/plain", "foobar.txt");
-// createFile("image/png", "mypicture.png");
-
-    // Unique request code.
-    private static final int WRITE_REQUEST_CODE = 43;
-    private void createFile(String mimeType, String fileName) {
-        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-
-        // Filter to only show results that can be "opened", such as
-        // a file (as opposed to a list of contacts or timezones).
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        // Create a file with the requested MIME type.
-        intent.setType(mimeType);
-        intent.putExtra(Intent.EXTRA_TITLE, fileName);
-        startActivityForResult(intent, WRITE_REQUEST_CODE);
     }
 
 }
